c9c9591faa87d0dee685bbbd4d6d31a3ea90abe1
Your Name <you@example.com>
2023-02-18 10:34:01 +0800

    2023-2-18 10:34

---
+++ contracts/dogechain_quickswap/Query.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ~0.8.17;
+import "./interfaces/IQuoter.sol";
+import "./interfaces/IERC20.sol";
+import "./libraries/SafeMath.sol";
+
+contract Query {
+    using SafeMath for uint256;
+    address public immutable quoter =
+        0xd8E1E7009802c914b0d39B31Fc1759A865b727B1;
+
+    function quote(
+        uint8 lv,
+        uint256 buyQty,
+        bytes calldata buyPath,
+        uint256 sellQty,
+        bytes calldata sellPath
+    ) external returns (uint256[] memory) {
+        uint256[] memory amounts = new uint256[](lv * 2);
+
+        if (buyQty > 0) {
+            for (uint8 i = 0; i < lv; i++) {
+                (amounts[i], ) = IQuoter(quoter).quoteExactOutput(
+                    buyPath,
+                    buyQty.mul(i + 1)
+                );
+            }
+        }
+
+        if (sellQty > 0) {
+            for (uint8 i = lv; i < lv * 2; i++) {
+                (amounts[i], ) = IQuoter(quoter).quoteExactOutput(
+                    sellPath,
+                    sellQty.mul(i + 1 - lv)
+                );
+            }
+        }
+
+        return amounts;
+    }
+
+    function getTimestamp() public view returns (uint256) {
+        return block.timestamp;
+    }
+
+    function getBlockNum() public view returns (uint256) {
+        return block.number;
+    }
+
+    function getBal(address token, address act) public view returns (uint256) {
+        return IERC20(token).balanceOf(act);
+    }
+
+    function getBals(address[] memory tokens, address act)
+        public
+        view
+        returns (uint256[] memory)
+    {
+        uint256[] memory bals = new uint256[](tokens.length);
+        for (uint8 i = 0; i < tokens.length; i++) {
+            bals[i] = IERC20(tokens[i]).balanceOf(act);
+        }
+
+        return bals;
+    }
+}
---
+++ contracts/dogechain_quickswap/libraries/SafeMath.sol
@@ -0,0 +1,92 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ~0.8.17;
+
+library SafeMath {
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return sub(a, b, "SafeMath: subtraction overflow");
+    }
+
+    function sub(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        require(b <= a, errorMessage);
+        uint256 c = a - b;
+
+        return c;
+    }
+
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+
+        return c;
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return div(a, b, "SafeMath: division by zero");
+    }
+
+    function div(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        // Solidity only automatically asserts when dividing by 0
+        require(b > 0, errorMessage);
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+
+        return c;
+    }
+
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        return mod(a, b, "SafeMath: modulo by zero");
+    }
+
+    function mod(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        require(b != 0, errorMessage);
+        return a % b;
+    }
+
+    function min(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a <= b ? a : b;
+    }
+
+    function max(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a >= b ? a : b;
+    }
+
+    function sqrt(uint256 a) internal pure returns (uint256 b) {
+        if (a > 3) {
+            b = a;
+            uint256 x = a / 2 + 1;
+            while (x < b) {
+                b = x;
+                x = (a / x + x) / 2;
+            }
+        } else if (a != 0) {
+            b = 1;
+        }
+    }
+}
